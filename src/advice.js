0ac7dbbd49f9d5cd4ac084a8dad8036a35907315
Alin-IlieM <alin.ilie_moldovan@yahoo.com>
2023-01-21 19:09:03 +0200

    cleaned up some code

--- src/advice.js
+++ src/advice.js
@@ -3,26 +3,20 @@ import 'tachyons';
 
 const Advice = (props) => {
     const {count} = props;
-    if (count !==0 && count%3 === 0) {
-        return (
-            <div className = 'tc pa4 georgia br-pill shadow-4 bg-washed-green'>
-                <h1>Here is your advice:</h1>
-                <p>{props.advice}</p>
-            </div>
-        )
+    const hasAdvice = count !== 0 && count % 3 === 0;
+    let advice = "You didn't earn your next advice";
+    if (hasAdvice) {
+        advice= "Here is your advice:";
     } else if (count < 3) {
-        return (
-            <div className = 'tc pa4 georgia br-pill shadow-4 bg-washed-green'>
-                <h1>You didn't earn your advice yet</h1>
-            </div>
-        )
-    } else {
-        return (
-            <div className = 'tc pa4 georgia br-pill shadow-4 bg-washed-green'>
-                <h1>You didn't earn your next advice yet</h1>
-            </div>
-        )
-    }
+        advice = "You didn't earn your advice yet"
+    } 
+
+    return (
+    <div className="tc pa4 georgia br-pill shadow-4 bg-washed-green">
+      <h1>{ advice }</h1>
+      {hasAdvice && <p>{props.advice}</p>}
+    </div>
+    )
         
 }
 
